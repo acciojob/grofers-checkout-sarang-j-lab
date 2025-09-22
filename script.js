ee2ea0917033a5630b74b003b84bcbd59223cd01
Sarang <149196971+sarang-j-lab@users.noreply.github.com>
2025-09-22 12:50:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
-};
+    const prices = document.querySelectorAll(".price");
+    let total = 0;
+    prices.forEach((ele)=>{
+        total += Number(ele.textContent)
+    })
+    const table = document.querySelector("table")
+    const tr = document.createElement("tr");
+    const td = document.createElement("td");
+    td.textContent = "Total is:- " +total;
+    tr.append(td); 
+    table.append(tr);
+};  
 
 getSumBtn.addEventListener("click", getSum);
 
